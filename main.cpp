d0a0f3092c7c9b2207dc38a741cc93863899fa58
hgalindo1610 <hgalindo@unitec.edu>
2017-02-03 07:31:03 -0600

    Tarea 2

--- main.cpp
+++ main.cpp
@@ -5,31 +5,48 @@ using namespace std;
 //Desrefencia ptr (dado) y devuelve su valor
 int obtenerValor(int *ptr)
 {
-    return -1;
+    return *ptr;
 }
 
 //Desreferencia a (dado) y b (dado) y devuelve la suma de ambos
 int sumar(int* a, int* b)
 {
-    return -1;
+    return *a + *b;
 }
 
 //Devuelve true si a (dado) y b (dado) apuntan al mismo espacio de memoria
 bool compararApuntadores(string *a, string *b)
 {
-    return false;
+ if (*a==*b)
+    {
+       return true;
+    }
+    else
+    {
+         return false;
+    }
+
 }
 
 //Desrefencia a (dado) y b (dado), devuelve true si ambos valores son iguales de lo contrario devuelve false
 bool comparar(string *a, string *b)
 {
-    return false;
+    if (*a==*b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
 
 //Desreferencia a (dado) y devuelve su primera letra
 char getPrimeraLetra(string* a)
 {
-    return ' ';
+  string cadena = *a;
+  return cadena[0];
 }
 
 //Desreferencia a (dado) y devuelve true si es palindroma de lo contrario devuelve false
@@ -37,13 +54,27 @@ char getPrimeraLetra(string* a)
 //Ejemplos de palindromos: Ana, arenera, arepera, anilina, ananá, Malayalam, Neuquén, Oruro, oso, radar, reconocer, rotor, salas, seres, somos, sometemos
 bool esPalindromo(string* a)
 {
-    return false;
+    string cadena = *a;
+     string aux_cadena = "";
+       for (int i = cadena.size()-1; i >=0 ; i--)
+    {
+        aux_cadena += cadena[i];
+    }
+       if (aux_cadena==cadena)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
 }
 
 //Desreferencia base (dado) y exponente (dado) y devuelve la base elevado al exponente
 int getExponente(int* base, int* exponente)
 {
-    return -1;
+   return -1;
 }
 
 int main ()
